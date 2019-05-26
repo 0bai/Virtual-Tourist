41430ba0133a2e3bf8425be0eca1f7d2b9fdf140
Obai Alnajjar <obai@outlook.com>
2019-05-26 09:06:05 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Obai Alnajjar on 5/26/19.
+//  Copyright © 2019 Obai Alnajjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
