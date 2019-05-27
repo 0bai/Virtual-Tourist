cf2c586bed25b747d575a53896d0411a5117cd29
Obai Alnajjar <obai@outlook.com>
2019-05-27 10:34:39 +0300

    Split Connection Manager file into helpers and ConnectionManager

---
+++ Virtual Tourist/Models/Helpers.swift
@@ -0,0 +1,66 @@
+//
+//  Helpers.swift
+//  Virtual Tourist
+//
+//  Created by Obai Alnajjar on 5/27/19.
+//  Copyright © 2019 Obai Alnajjar. All rights reserved.
+//
+
+import Foundation
+
+extension ConnectionManager{
+    
+    internal static func fireRequest(url:URL, headers:[String:String]?, body:Data?, responseHandler:@escaping (_ data:Data, _ response:URLResponse?, _ error:Error?)->()) {
+        
+        var request = URLRequest(url: url)
+        
+        headers?.forEach{(key, value) in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        request.httpBody = body
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+                self.delegate?.serverError(error: "Connection Error", details: "Please check your internet connection!")
+                return
+            }
+            
+            switch statusCode {
+            case 200 ... 299 :
+                responseHandler(data!, response, error)
+            default :
+                self.delegate?.serverError(error: "Server Error", details: "Something went wrong!")
+                
+            }
+        }
+        
+        task.resume()
+    }
+    
+    internal static func encode<T:Codable>(object:T) -> Data{
+        
+        let encoder = JSONEncoder()
+        do {
+            let json = try encoder.encode(object)
+            return json
+        } catch {
+            self.delegate?.serverError(error: "Internal Error", details: "something went wrong while wrapping the data!")
+            return error as! Data
+        }
+    }
+    
+    internal static func decode<T: Codable>(data:Data, type:T.Type) -> Codable{
+        do {
+            let decoder = JSONDecoder()
+            let genericObject =  try decoder.decode(type.self, from: data)
+            return genericObject
+        } catch  {
+            print("error while decoding \(type)")
+            delegate?.serverError(error: "Internal Error", details: "Error while unwrapping the data!")
+            return error as! Codable
+        }
+    }
+}
